fbc04b3033554c0bb562cf1e56a81cf3975667cf
Hugo Latreille <hugo.latreille@gmail.com>
2022-06-09 15:36:45 +0200

    test debug route API from client

--- public/src/utils/APIroutes.js
+++ public/src/utils/APIroutes.js
@@ -1,5 +1,6 @@
 // export const host = "http://localhost:8888";
-export const host = `http://localhost:${process.env.PORT}`;
+// export const host = `http://localhost:${process.env.PORT}`
+export const host = `http://localhost`;
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
